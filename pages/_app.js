7dcb85a391cc274b7b0ec88b9e7e961fff22f75e
Luc Rousseau <luc.rousseau@strongdeveloppement.ca>
2024-03-05 22:06:32 +0100

    various

--- pages/_app.js
+++ pages/_app.js
@@ -2,9 +2,12 @@ import "../utils/fortawesome";
 
 import "../styles/main.scss";
 
+import { SpeedInsights } from "@vercel/speed-insights/next";
+
 import { appWithTranslation } from "next-i18next";
 
 function MyApp({ Component, pageProps }) {
+  <SpeedInsights />;
   return <Component {...pageProps} />;
 }
 
