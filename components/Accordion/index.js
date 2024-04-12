6e7592cbccb63aa9dac5ae387c3eb7c54177ec1f
Luc Rousseau <luc.rousseau@strongdeveloppement.ca>
2024-04-12 08:25:17 +0200

    add + for mobile

--- components/Accordion/index.js
+++ components/Accordion/index.js
@@ -40,7 +40,35 @@ export default function Accordion({ className, items, ...props }) {
           })}
         >
           <h3 onClick={() => toggleItem(index)}>
-            {item.title} {item.emoji && <i>{item.emoji}</i>}
+            {item.title}{" "}
+            {item.emoji && (
+              <i>
+                {item.emoji}
+                <em>
+                  {activeIndex !== index ? (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 448 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z"
+                      />
+                    </svg>
+                  ) : (
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 448 512"
+                    >
+                      <path
+                        fill="currentColor"
+                        d="M432 256c0 17.7-14.3 32-32 32L48 288c-17.7 0-32-14.3-32-32s14.3-32 32-32l352 0c17.7 0 32 14.3 32 32z"
+                      />
+                    </svg>
+                  )}
+                </em>
+              </i>
+            )}
           </h3>
           <div className="component__accordion__content">{item.content}</div>
         </div>
