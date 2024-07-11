12a5a0dd6b69fd486fd14f21e49c367d99984525
Luc Rousseau <luc.rousseau@strongdeveloppement.ca>
2024-07-11 15:44:04 -0400

    added @next/bundle-analyzer

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,13 @@
 
 import i18n from "./next-i18next.config.js";
 
+const withBundleAnalyzer = (await import("@next/bundle-analyzer")).default({
+  enabled: process.env.ANALYZE === "true",
+});
+
 const nextConfig = {
   ...i18n,
   reactStrictMode: true,
 };
 
-export default nextConfig;
+export default withBundleAnalyzer(nextConfig);
