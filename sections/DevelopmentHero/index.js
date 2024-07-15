d42ed89fdcff987f4559ffd0192d1582c085a2e5
Luc Rousseau <luc.rousseau@strongdeveloppement.ca>
2024-07-15 15:12:34 -0400

    copy update

--- sections/DevelopmentHero/index.js
+++ sections/DevelopmentHero/index.js
@@ -98,7 +98,7 @@ export default function DevelopmentHero() {
                       content: (
                         <>
                           <Buy
-                            price={300}
+                            price={175}
                             legend={t("development-hero:buy:legend")}
                             prefix={parse(t("development-hero:buy:prefix"))}
                             label={t("common:schedule-me-label")}
